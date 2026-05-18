a0c9564f0a701ed07032bc1a290e1e5ba2c4119e
Sindy <sindy.nieto@gmail.com>
2026-05-18 10:36:58 -0600

    cambio1

--- gestion_de_gastos/src/App.jsx
+++ gestion_de_gastos/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import HomePage from "./pages/home/HomePage"
+import HomePage from "./pages/Home/HomePage"
 import Equipo from "./pages/Equipo/EquipoPage"
 import Crud from "./pages/Crud/Crud"
 import MainLayout from "./layout/MainLayout"
@@ -13,10 +13,11 @@ function App() {
       <MainLayout>
         <Routes>
           <Route path="/" element={<HomePage/>} />
+          <Route path="/home" element={<HomePage/>} />
           <Route path="/Equipo" element={<Equipo/>} />
           <Route path="/Crud" element={<Crud/>} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </MainLayout>
     </Router>
